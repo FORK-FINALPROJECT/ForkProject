5be6dad52adca2cba11175d06ba91d7f7359b268
saeha_kim <kimsaehaa@gmail.com>
2023-09-20 11:24:02 +0900

    테스트

--- fork/src/main/java/com/fork/HomeController.java
+++ fork/src/main/java/com/fork/HomeController.java
@@ -8,9 +8,60 @@ public class HomeController {
 
 	@GetMapping("/")
 	public String home() {
-		
 		return "forward:index.jsp";
-		
 	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "common/login";
+	}
+	
+	@GetMapping("/companyInfo")
+	public String companyInfo() {
+		return "common/companyInfo";
+	}
+	
+	@GetMapping("/contectUs")
+	public String contectUs() {
+		return "common/contectUs";
+	}
+	
+	@GetMapping("/shopInfo")
+	public String shopInfo() {
+		return "common/shopInfo";
+	}
+	
+//	@GetMapping("/noticeListView")
+//	public String noticeListView() {
+//		return "notice/noticeListView";
+//	}
+	
+	@GetMapping("/qnaListView")
+	public String qnaListView() {
+		return "qna/qnaListView";
+	}
+	
+	@GetMapping("/adSignupApproval")
+	public String adSignupApproval() {
+		return "common/adSignupApproval";
+	}
+	
+	@GetMapping("/adShopList")
+	public String adShopList() {
+		return "common/adShopList";
+	}
+	
+	@GetMapping("/adNoticeListView")
+	public String adNoticeListView() {
+		return "adNotice/adNoticeListView";
+	}
+	
+	@GetMapping("/adQnaListView")
+	public String adQnaListView() {
+		return "adQna/adQnaListView";
+	}
+	
+	
+	
 	
 }
